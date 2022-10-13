a1566f9f52590c0a35674489d8b67382b0c09a7c
lambchop96 <justinkalam@gmail.com>
2022-10-13 10:54:57 -0700

    Fixed two methods in ArrayExamples.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace1() {
+    int[] input1 = { 3, 6, 9 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 9, 6, 3 }, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = { 7, 8, 9 };
+    assertArrayEquals(new int[]{ 9, 8, 7 }, ArrayExamples.reversed(input1));
   }
 }
